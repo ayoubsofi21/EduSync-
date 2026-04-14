e9f2d66b63958fe55c5f0e78409b94a17f81a6d2
ayoubsofi <ayoub@email.com>
2026-04-14 15:47:34 +0100

    create classes table and roles

--- seed.sql
+++ seed.sql
@@ -1,2 +1,6 @@
-CREATE TABLE users(id int PRIMARY KEY AUTO_INCREMENT ,firstname varchar(100),lastname varchar(100),email varchar(100) UNIQUE,passwordl varchar(100),role_id int,Foreign Key (role_id) REFERENCES roles(id))
+use DATABASE EDUSYNC;
+CREATE roles(id int PRIMARY KEY AUTO_INCREMENT ,label varchar(255))
+CREATE TABLE users(id int PRIMARY KEY AUTO_INCREMENT ,firstname varchar(100),lastname varchar(100),email varchar(100) UNIQUE,passwordl varchar(100),role_id int,Foreign Key (role_id) REFERENCES roles(id));
+CREATE TABLE classes(id int PRIMARY KEY AUTO_INCREMENT ,name varchar(100),classroom_number varchar(255)) ;
+
 
